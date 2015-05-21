b860c93b52d2a912f2df5b30121c55f20ea9acb8
Phil <philip.dasler@gmail.com>
2015-05-21 00:10:21 -0400

    Final version

--- weather_shirt.ino
+++ weather_shirt.ino
@@ -9,14 +9,16 @@
 #define SUN 1
 #define HOT 2
 #define MUGGY 3
-#define RAIN 4
-#define END_OF_STATES 5
+#define CLOUDY 4
+#define RAIN 5
+#define END_OF_STATES 4
 
 // Mode select button
-#define BUTTON 4
+#define BUTTON 3
+#define DUMMY_VOLTS 2
 
 // Humidity sensor
-#define DHTPIN 2     // what pin humidity is connected to
+#define DHTPIN 4     // what pin humidity is connected to
 #define DHTTYPE DHT11   // DHT 11 
 
 // LCD Screen pins
@@ -29,8 +31,21 @@
 #define HEATING_PAD_1 5
 #define HEATING_PAD_2 6
 
+// Pressure Sensor
+/* 
+SDA to I2C Data (Analog 4)
+SCL to I2C Clock (Analog 5)
+*/
+
 // Current mode
 int mode = 0;
+
+// State Variables
+boolean humid = false;
+boolean hot = false;
+
+// Thresholds
+const int humidity_thresh = 44;
 
 // Initialize DHT sensor for normal 16mhz Arduino
 DHT dht(DHTPIN, DHTTYPE);
@@ -49,12 +64,19 @@ float pressure; // pressure, duh
 // calculated values
 float hi; // heat index
 
+int sweatX = 110;
+int sweatY = 90;
+
 
 void setup() {
   pinMode( BUTTON, INPUT );
   pinMode( HEATING_PAD_1, OUTPUT );
   pinMode( HEATING_PAD_2, OUTPUT );
-
+  
+  // For supplying a test voltage to the button
+  pinMode( DUMMY_VOLTS, OUTPUT );
+  digitalWrite( DUMMY_VOLTS, HIGH );
+  
   graphic.Init(0,1,0,1,1); //Revesed X direction.
 
   clearImage();  
@@ -68,14 +90,25 @@ void setup() {
     while (1) {
     }
   }
+  
+  digitalWrite( HEATING_PAD_1, HIGH );
+  digitalWrite( HEATING_PAD_2, HIGH );
 
+  for( int x=0; x<END_OF_STATES; x++ ) {
+    advanceMode();
+    delay(3000);
+  }
+    
+  changeMode(0);
 }
 
 void loop() {
+  /*
   if( digitalRead(BUTTON) ) {
     advanceMode();
     delay(250);
   }
+*/
 
   if( mode == SENSOR_READING ) {
     readSensors();
@@ -84,10 +117,17 @@ void loop() {
 }
 
 void advanceMode() {
+  int prev_mode = mode;
+  
   mode++;
   if( mode == END_OF_STATES )
     mode = 0;
 
+  Serial.print("Changing mode from mode ");
+  Serial.print(prev_mode);
+  Serial.print(" to ");
+  Serial.print(mode);
+  Serial.println(".");
   handleModeChange();
 }
 
@@ -97,11 +137,14 @@ void changeMode( int new_mode ){
 }
 
 void handleModeChange() {
-  clearImage();
-  analogWrite( HEATING_PAD_1, 0 );
-  analogWrite( HEATING_PAD_2, 0 );
+  //clearImage();
+  digitalWrite( HEATING_PAD_1, HIGH );
+  digitalWrite( HEATING_PAD_2, HIGH );
 
   switch( mode ) {
+  case SENSOR_READING:
+    drawSun();
+    break;
   case SUN:
     drawSun();
     break;
@@ -109,21 +152,46 @@ void handleModeChange() {
     drawSunglasses();
     break;
   case MUGGY:
-    drawSweat(120, 65);
+    drawSweat(sweatX, sweatY);
     break;
   case RAIN:
     drawRain();
+    break;  
+  case CLOUDY:
+    drawSun();
+    drawCloud();
     break;
   }
 }
 
 void processSensors() {
-
+  if( !humid && hum > humidity_thresh) {
+    humid = true;
+    drawSweat(sweatX, sweatY);
+  }
+  
+  if( humid && hum <= humidity_thresh ) {
+    humid = false;
+    drawSun();
+  }
+  
+  if ( !hot && temp > 75 ) {
+    hot = true;
+    drawSunglasses();
+  }
+  
+   if ( hot && temp <= 75 ) {
+    hot = false;
+    drawSun();
+  }
 }
 
 void drawRain() {
-  analogWrite( HEATING_PAD_1, 255 );
-  analogWrite( HEATING_PAD_2, 255 );
+  digitalWrite( HEATING_PAD_1, LOW );
+}
+
+void drawCloud() {
+  digitalWrite( HEATING_PAD_2, LOW );
 }
 
 void readSensors() {
@@ -167,41 +235,51 @@ void clearImage() {
 }
 
 void drawSun() {
+  Serial.println("Sun");
   graphic.SetForeColour(15,15,0);
   graphic.Circle(65,65,64,5); 
 }
 
 void drawSunglasses(){
   graphic.SetForeColour(0,0,0);
-  graphic.Circle(42,55,18,5); 
-  graphic.Circle(88,55,18,5); 
+  
+  // Lenses
+  graphic.Circle(75,42,18,5); 
+  graphic.Circle(75,88,18,5);
 
-  graphic.Box(42,48,88,53,5);
+  // Cross piece
+  graphic.Box(77,42,82,88,5);
 
-  graphic.Box(0,49,30,52,5);
-  graphic.Box(100,49,130,52,5);
+  // Ear pieces
+  graphic.Box(78,0,81,30,5);
+  graphic.Box(78,100,81,130,5);
 
+  // Square off lenses
   graphic.SetForeColour(15,15,0);
-  graphic.Box(20,30,60,47,5);
-  graphic.Box(70,30,110,47,5);
+  graphic.Box(83,20,100,60,5);
+  graphic.Box(83,70,100,110,5);
 
+  // Reflections
   graphic.SetForeColour(15,15,15);
-  graphic.Box(35,51,39,52,5);
-  graphic.Box(75,51,79,52,5);
+  graphic.Box(78,35,79,39,5);
+  graphic.Box(78,75,79,79,5);
 }
 
 void drawSweat(int x, int y) {
   graphic.SetForeColour(0, 0, 15);
-  graphic.Circle(x,y+3,6,5); 
+  graphic.Circle(y+3,x,6,5); 
 
   for(int i=0; i<5; i++){
-    graphic.Line(x-6,y+i,x,y-7+i,1);
-    graphic.Line(x+6,y+i,x,y-7+i,1);
+    graphic.Line(y+i,x-6,y+7+i,x,1);
+    graphic.Line(y+i,x+6,y+7+i,x,1);
   }
 
   graphic.SetForeColour(15, 15, 15);
-  graphic.Line(x-3, y, x-2, y-1, 1);
+  graphic.Line(y+5, x-3, y+6, x-2, 1);
 }
+
+
+
 
 
 
